4955dddcbbbfdf4068c8d901d621c9fc3b8f5dc6
Kaung Set <kset2299@gmail.com>
2024-08-17 17:09:37 +0630

    first commit

---
+++ tailwind.config.mjs
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        deepNavy: "#12172B",
+        charcoal: "#1A1A1A",
+        offWhite: "#E8E8E8",
+        oceanBlue: "#007BFF",
+        softWhite: "#F5F5F5",
+        lightGray: "#BFBFBF",
+      },
+    },
+  },
+  plugins: [],
+};
